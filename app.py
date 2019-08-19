4a25d9d554bffa9214097b9ee87c54f8fff10c1e
kuokuanyo <a167829435@gmail.com>
2019-08-19 09:57:00 +0800

    new

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def handle_message(event):
         r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text='吃飯了嗎'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
