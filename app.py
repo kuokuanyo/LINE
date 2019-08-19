e068964e986af2927878ab0671451dd59717453f
kuokuanyo <a167829435@gmail.com>
2019-08-19 09:55:43 +0800

    new

--- app.py
+++ app.py
@@ -43,6 +43,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r = '我看不懂你說什麼'
+    if msg =='hi':
+        r = 'hi'
+    elif msg == '你吃飯了嗎':
+        r = '還沒'
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text='吃飯了嗎'))
