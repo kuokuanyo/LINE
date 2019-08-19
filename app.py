638ad7e30fcd263c4a7e99df3d4913f23ebe9623
kuokuanyo <a167829435@gmail.com>
2019-08-19 10:13:25 +0800

    new

--- app.py
+++ app.py
@@ -47,12 +47,13 @@ def handle_message(event):
 
     if '貼圖' in msg:
         sticker_message = StickerSendMessage(
-        package_id='1',
-        sticker_id='1'
+            package_id='1',
+            sticker_id='1'
         )
+        
         line_bot_api.reply_message(
-        event.reply_token,
-        sticker_message)
+            event.reply_token,
+            sticker_message)
         return
 
     if msg in ['hi','Hi']:
